565ec5eafa364334bbe75636ff9a563b735a8023
Ezequiel Marin <ezequiel.marin@mi.unc.edu.ar>
2024-04-04 17:17:06 -0300

    Estructura general de connection.py, creo metodo selector de comandos

--- connection.py
+++ connection.py
@@ -16,10 +16,54 @@ class Connection(object):
 
     def __init__(self, socket, directory):
         # FALTA: Inicializar atributos de Connection
-        pass
+        self.socket = socket
+        self.directory = directory
+
+    def quit(self):
+        """
+        Termina la conexión con el cliente.
+        """
+        status = CODE_OK
+        self.socket.send(b"Bye!\n")
+        self.socket.close()
+        
+    def get_file_listing(self):
+        """
+        Devuelve una lista de los archivos en el directorio del servidor.
+        """
+        return 
+    
+    def get_metadata(self, filename):
+        """
+        Devuelve un diccionario con los metadatos del archivo filename.
+        """
+        return
+    
+    def get_slice(self, filename, offset, size):
+        """
+        Devuelve una parte del archivo filename, comenzando en offset y
+        con un tamaño de size bytes.
+        """
+        return
+    
+    
+# Creo un selector de comandos, que se encargará de llamar a los métodos correspondientes
+# cmd es un string que representa el comando a ejecutar
+    def cmd_selector(self, cmd):
+        """
+        Selecciona el comando a ejecutar segun el string cmd
+        """
+        if cmd == "quit":
+            self.quit()
+        elif cmd == "get":
+            self.get()
+        elif cmd == "quit":
+            self.quit()
+        else:
+            self.unknown()
 
     def handle(self):
         """
         Atiende eventos de la conexión hasta que termina.
         """
-        pass
+        # FALTA: Loop principal de la conexión\
